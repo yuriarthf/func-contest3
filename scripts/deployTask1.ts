d5693f77e02f09da015129eefd272d9736a308d6
yuriarthf <yuriarthf@protonmail.com>
2023-02-08 03:40:07 +0700

    feat: use create-ton template

---
+++ scripts/deployTask1.ts
@@ -0,0 +1,13 @@
+import { toNano } from 'ton-core';
+import { Task1 } from '../wrappers/Task1';
+import { compile, NetworkProvider } from '@ton-community/blueprint';
+
+export async function run(provider: NetworkProvider) {
+    const task1 = Task1.createFromConfig(await compile('Task1'));
+
+    await provider.deploy(task1, toNano('0.05'));
+
+    const openedContract = provider.open(task1);
+
+    // run methods on `openedContract`
+}
---
+++ wrappers/Task1.compile.ts
@@ -0,0 +1,5 @@
+import { CompilerConfig } from '@ton-community/blueprint';
+
+export const compile: CompilerConfig = {
+    targets: ['contracts/task_1.fc'],
+};
